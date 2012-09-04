6c623863f9e697102ceb8dd45704bc16f7990e3d
Simon Weber <sweb090@gmail.com>
2012-09-04 03:12:27 -0400

    linting

--- tla.py
+++ tla.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from flask import Flask, request
 import requests
@@ -7,7 +8,8 @@ ENV_KEYS = ('GH_USER', 'GH_SECRET', 'CIO_KEY', 'CIO_SECRET')
 
 app = Flask(__name__)
 app.config['GH_ROOT'] = 'https://api.github.com/'
-app.debug=True
+app.debug = True
+
 
 def load_env_conf(keys=ENV_KEYS):
     """A hack, since I'm using Heroku env files + foreman."""
@@ -16,6 +18,7 @@ def load_env_conf(keys=ENV_KEYS):
         app.config[key] = os.environ[key]
 
 load_env_conf()
+
 
 @app.route('/tlpost', methods=['POST'])
 def tlpost():
@@ -29,6 +32,7 @@ def tlpost():
     #TODO convert to html
     #then create_gh_commit(...)
 
+
 @app.route('/mailfailure')
 def mailfailure():
     """GET by context.IO if the WebHook fails and will no longer be active."""
@@ -41,39 +45,43 @@ def mailfailure():
     return "ok"
 
 
-def create_gh_commit(user, passwd, repo, 
-        filepath, contents, commit_message, 
+def create_gh_commit(user, passwd, repo,
+        filepath, contents, commit_message,
         executable=False, force=False):
-    """Create a new commit on GitHub. See http://developer.github.com/v3/git/."""
+    """Create a new commit on GitHub.
+    See http://developer.github.com/v3/git/."""
 
     #Used both:
     # http://www.pqpq.de/2011/07/pithub-how-to-commit-new-file-via.html
     # http://swanson.github.com/blog/2011/07/23/digging-around-the-github-api-take-2.html
     #as a reference here. Neither were totally correct.
 
-    app.loggger.debug("will commit %s",filepath)
+    app.loggger.debug("will commit %s", filepath)
 
     GH_ROOT = app.config['GH_ROOT']
 
-    sha_latest_commit = requests.get(GH_ROOT+"repos/{user}/{repo}/git/refs/heads/master".format(
+    sha_latest_commit = requests.get(
+            GH_ROOT + "repos/{user}/{repo}/git/refs/heads/master".format(
         user=user,
         repo=repo)
         ).json['object']['sha']
 
-    sha_base_tree = requests.get(GH_ROOT+"repos/{user}/{repo}/git/commits/{sha}".format(
+    sha_base_tree = requests.get(
+            GH_ROOT + "repos/{user}/{repo}/git/commits/{sha}".format(
         user=user,
         repo=repo,
         sha=sha_latest_commit)
         ).json['tree']['sha']
 
-    sha_new_tree = requests.post(GH_ROOT+"repos/{user}/{repo}/git/trees".format(
+    sha_new_tree = requests.post(
+            GH_ROOT + "repos/{user}/{repo}/git/trees".format(
         user=user,
         repo=repo
         ),
-        auth = (user, passwd),
-        data = json.dumps({
-            'base_tree':sha_base_tree,
-            'tree':[
+        auth=(user, passwd),
+        data=json.dumps({
+            'base_tree': sha_base_tree,
+            'tree': [
                 {
                     'path': filepath,
                     'mode': '100755' if executable else '100644',
@@ -84,25 +92,27 @@ def create_gh_commit(user, passwd, repo,
             })
         ).json['sha']
 
-    sha_new_commit = requests.post(GH_ROOT+"repos/{user}/{repo}/git/commits".format(
+    sha_new_commit = requests.post(
+            GH_ROOT + "repos/{user}/{repo}/git/commits".format(
         user=user,
         repo=repo
         ),
-        auth = (user, passwd),
-        data = json.dumps({
+        auth=(user, passwd),
+        data=json.dumps({
             'message': commit_message,
-            'parents':[sha_latest_commit],
+            'parents': [sha_latest_commit],
             'tree': sha_new_tree,
             })
         ).json['sha']
 
-    app.logger.debug(requests.patch(GH_ROOT+"repos/{user}/{repo}/git/refs/heads/master".format(
+    app.logger.debug(requests.patch(
+        GH_ROOT + "repos/{user}/{repo}/git/refs/heads/master".format(
         user=user,
         repo=repo
         ),
-        auth = (user, passwd),
-        data = json.dumps({
-            'sha':sha_new_commit,
+        auth=(user, passwd),
+        data=json.dumps({
+            'sha': sha_new_commit,
             'force': force
             })
         ).json)
